94a975c1c07e33f303983bb3c501066430b66367
danessti <danessti@gmail.com>
2023-08-31 19:57:00 -0500

    controladores y servidores agregados al proyecto

---
+++ src/main/java/com/example/ProyectoIntegradorMakaia/Entities/Aeropuertos.java
@@ -0,0 +1,9 @@
+package com.example.ProyectoIntegradorMakaia.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "aeropuertos")
+public class Aeropuertos {
+}
